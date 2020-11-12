6e59f820ec68740622217be404478fbca2b2fc5c
abiradar27 <abiradar9@gmail.com>
2020-11-12 20:41:58 +0530

    first commit

---
+++ test_code/demo1.py
@@ -0,0 +1,19 @@
+import sys
+print("Hello ABC client")
+age=27
+a='ABC for TECH'
+print(a[0:3])
+print(a[:3])
+print(a[0:])
+print(a[::2])
+print(a[::3])
+print(a)
+print(a[4])
+print(a[-7])
+print(type(age))
+print("ABC for tech")
+
+print(sys.argv[0])
+# print(sys.argv[1])
+
+# print(sys.argv[2])
